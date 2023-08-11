71c6c8851a8d8e757028b2f24b6524fb5270ab47
“cliveclistar” <clivemadungwe281@gmail.com>
2023-08-11 02:07:07 -0700

    c programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *return - always 0
+  */
+int main(void)
+{
+	printf("Size of char: %lu byte(s)", sizeof(char));
+	printf("Size of int: %lu byte(s)", sizeof(int));
+	printf("Size of long int: %lu byte(s)", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of float: %lu byte(s)", sizeof(float));
+	return(0);
+}
